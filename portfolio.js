e7880ef9dfd8e180ddb77634037e84c87837e765
Sunil Tiwari <cybertiwari@gmail.com>
2022-01-01 19:00:18 +0530

    init

---
+++ portfolio.js
@@ -0,0 +1,206 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Sunil Tiwari",
+	title: "Hi, I'am Sunil Tiwari",
+	description:
+		"A passionate Full Stack Web Developer and Mobile App Developer having an experience of building Web applications with Laravel / php / VueJs / InertiaJS / AWS and some other cool libraries and frameworks and Cross Platform Mobile Apps With Flutter.",
+	resumeLink: "https://drive.google.com/file/d/1HWX1cHplaSt6nuXYtUGboEcIlbtBvsu4/view",
+};
+
+export const openSource = {
+	githubUserName: "suniltiwari-dev",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	facebook: "https://www.facebook.com/cybertiwari",
+	instagram: "https://www.instagram.com/suniltiwari_dev/",
+	twitter: "https://twitter.com/suniltiwari_dev",
+	github: "https://github.com/suniltiwari-dev",
+	linkedin: "https://www.linkedin.com/in/cybertiwari/",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"PASSIONATE WEB APP DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+	skills: [
+		emoji(
+			"⚡ Develop highly interactive Web App for your Business"
+		),
+		emoji(
+			"⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"
+		),
+		emoji(
+			"⚡ Integration of third party services such as Firebase / AWS / Digital Ocean"
+		),
+	],
+
+	softwareSkills: [
+		{
+			skillName: "html-5",
+			fontAwesomeClassname: "vscode-icons:file-type-html",
+		},
+		{
+			skillName: "css3",
+			fontAwesomeClassname: "vscode-icons:file-type-css",
+		},
+		{
+			skillName: "sass",
+			fontAwesomeClassname: "logos:sass",
+		},
+		{
+			skillName: "JavaScript",
+			fontAwesomeClassname: "logos:javascript",
+		},
+		{
+			skillName: "flutter",
+			fontAwesomeClassname: "logos:flutter",
+		},
+		{
+			skillName: "composer",
+			fontAwesomeClassname: "vscode-icons:file-type-composer",
+		},
+		{
+			skillName: "sql-database",
+			fontAwesomeClassname: "vscode-icons:file-type-sql",
+		},
+		{
+			skillName: "mongoDB",
+			fontAwesomeClassname: "vscode-icons:file-type-mongo",
+		},
+		{
+			skillName: "aws",
+			fontAwesomeClassname: "logos:aws",
+		},
+		{
+			skillName: "firebase",
+			fontAwesomeClassname: "logos:firebase",
+		},
+		{
+			skillName: "php",
+			fontAwesomeClassname: "logos:php",
+		},
+		{
+			skillName: "git",
+			fontAwesomeClassname: "logos:git-icon",
+		},
+		{
+			skillName: "docker",
+			fontAwesomeClassname: "logos:docker-icon",
+		},
+		{
+			skillName: "laravel",
+			fontAwesomeClassname: "logos:laravel",
+		},
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "Web App Developement",
+		progressPercentage: "90", 
+	},
+	{
+		Stack: "DevOps",
+		progressPercentage: "70",
+	},
+	{
+		Stack: "Mobile App Development",
+		progressPercentage: "60",
+	},
+];
+
+export const educationInfo = [
+	{
+		schoolName: "MGSU Bikaner",
+		subHeader: "Bachelor of Science in Biology",
+		duration: "May 2016 - Nov 2020",
+		// desc: "Participated in the research of XXX and published 3 papers.",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		schoolName: "Prince School Sikar",
+		subHeader: "Senior Secondary",
+		duration: "May 2014 - April 2015",
+		// desc: "Participated in the research of XXX and published 3 papers.",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consectetfgur adipiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+	{
+		schoolName: "AVM School Nokha",
+		subHeader: "Secondary",
+		duration: "May 2012 - April 2013",
+		// desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+		// descBullets: [
+		// 	"Lorem ipsum dolorfdg sit amet, consectetur adipiscing elit",
+		// ],
+	},
+];
+
+export const experience = [
+	{
+		role: "PHP Developer Intern",
+		company: "Axixa Technologies",
+		companylogo: "/img/icons/common/axixa.png",
+		date: "July 2019 – Oct 2019",
+		// desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+	{
+		role: "Software Engineer",
+		company: "Taknik Bharti",
+		companylogo: "/img/icons/common/tb.png",
+		date: "Nov 2019 – July 2021",
+		// desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+	{
+		role: "Software Engineer",
+		company: "Sunarc Technologies",
+		companylogo: "/img/icons/common/sunarc.png",
+		date: "Aug 2021 – Present",
+		// desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		// descBullets: [
+		// 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		// ],
+	},
+];
+
+export const projects = [
+	{
+		name: "Literate",
+		desc: "A Complete 360 School Management Software",
+		github: "https://github.com/suniltiwari-dev/LIterate",
+		link: "https://github.com/suniltiwari-dev/LIterate",
+	},
+	{
+		name: "LoopSpot",
+		desc: "A plateform where user receive tailored recommendations, follow your friends, and receive rewards for thier reviews.",
+		link: "https://www.loopspot.com/",
+	},
+	{
+		name: "Axixa Technologies",
+		desc: "Education Portfolio website",
+		link: "http://axixatechnologies.com/",
+	},
+];
+
+export const feedbacks = [
+	{
+		name: "Satish Pareek",
+		feedback:
+			"We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Sunil Tiwari has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose Sunil web developer services for u.",
+	},
+	{
+		name: "Sunil Tiwari",
+		feedback:
+			"the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
+	},
+];
